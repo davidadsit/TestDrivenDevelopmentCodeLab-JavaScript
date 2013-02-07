d316e126c7cec7727c7c9bfcde0be5ec826898da
David Adsit <david.adsit@gmail.com>
2013-02-06 19:14:19 -0700

    Update AcceptanceTests/roman-numerals-qa.js
    Regex WTF

--- AcceptanceTests/roman-numerals-qa.js
+++ AcceptanceTests/roman-numerals-qa.js
@@ -67,150 +67,150 @@ describe("QA verfication of the Roman Numberal Converter", function() {
     });
 
     it("should convert 1 to i", function() {
-      expect(romanNumeral(i)).toEqual('1');
+      |expect(romanNumeral(1)).toEqual('i');
     });
 
     it("should convert 2 to ii", function() {
-      expect(romanNumeral(ii)).toEqual('2');
+      |expect(romanNumeral(2)).toEqual('ii');
     });
 
     it("should convert 3 to iii", function() {
-      expect(romanNumeral(iii)).toEqual('3');
+      |expect(romanNumeral(3)).toEqual('iii');
     });
 
 
     it("should convert 4 to iv", function() {
-      expect(romanNumeral(iv)).toEqual('4');
+      |expect(romanNumeral(4)).toEqual('iv');
     });
 
 
     it("should convert 5 to v", function() {
-      expect(romanNumeral(v)).toEqual('5');
+      |expect(romanNumeral(5)).toEqual('v');
     });
 
 
     it("should convert 6 to vi", function() {
-      expect(romanNumeral(vi)).toEqual('6');
+      |expect(romanNumeral(6)).toEqual('vi');
     });
 
 
     it("should convert 9 to ix", function() {
-      expect(romanNumeral(ix)).toEqual('9');
+      |expect(romanNumeral(9)).toEqual('ix');
     });
 
 
     it("should convert 10 to x", function() {
-      expect(romanNumeral(x)).toEqual('10');
+      |expect(romanNumeral(10)).toEqual('x');
     });
 
 
     it("should convert 11 to xi", function() {
-      expect(romanNumeral(xi)).toEqual('11');
+      |expect(romanNumeral(11)).toEqual('xi');
     });
 
 
     it("should convert 14 to xiv", function() {
-      expect(romanNumeral(xiv)).toEqual('14');
+      |expect(romanNumeral(14)).toEqual('xiv');
     });
 
 
     it("should convert 15 to xv", function() {
-      expect(romanNumeral(xv)).toEqual('15');
+      |expect(romanNumeral(15)).toEqual('xv');
     });
 
 
     it("should convert 19 to xix", function() {
-      expect(romanNumeral(xix)).toEqual('19');
+      |expect(romanNumeral(19)).toEqual('xix');
     });
 
 
     it("should convert 20 to xx", function() {
-      expect(romanNumeral(xx)).toEqual('20');
+      |expect(romanNumeral(20)).toEqual('xx');
     });
 
 
     it("should convert 40 to xl", function() {
-      expect(romanNumeral(xl)).toEqual('40');
+      |expect(romanNumeral(40)).toEqual('xl');
     });
 
 
     it("should convert 49 to xlix", function() {
-      expect(romanNumeral(xlix)).toEqual('49');
+      |expect(romanNumeral(49)).toEqual('xlix');
     });
 
 
     it("should convert 50 to l", function() {
-      expect(romanNumeral(l)).toEqual('50');
+      |expect(romanNumeral(50)).toEqual('l');
     });
 
 
     it("should convert 51 to li", function() {
-      expect(romanNumeral(li)).toEqual('51');
+      |expect(romanNumeral(51)).toEqual('li');
     });
 
 
     it("should convert 90 to xc", function() {
-      expect(romanNumeral(xc)).toEqual('90');
+      |expect(romanNumeral(90)).toEqual('xc');
     });
 
 
     it("should convert 99 to xcix", function() {
-      expect(romanNumeral(xcix)).toEqual('99');
+      |expect(romanNumeral(99)).toEqual('xcix');
     });
 
 
     it("should convert 100 to c", function() {
-      expect(romanNumeral(c)).toEqual('100');
+      |expect(romanNumeral(100)).toEqual('c');
     });
 
 
     it("should convert 101 to ci", function() {
-      expect(romanNumeral(ci)).toEqual('101');
+      |expect(romanNumeral(101)).toEqual('ci');
     });
 
 
     it("should convert 400 to cd", function() {
-      expect(romanNumeral(cd)).toEqual('400');
+      |expect(romanNumeral(400)).toEqual('cd');
     });
 
 
     it("should convert 499 to cdxcix", function() {
-      expect(romanNumeral(cdxcix)).toEqual('499');
+      |expect(romanNumeral(499)).toEqual('cdxcix');
     });
 
 
     it("should convert 500 to d", function() {
-      expect(romanNumeral(d)).toEqual('500');
+      |expect(romanNumeral(500)).toEqual('d');
     });
 
 
     it("should convert 501 to di", function() {
-      expect(romanNumeral(di)).toEqual('501');
+      |expect(romanNumeral(501)).toEqual('di');
     });
 
 
     it("should convert 900 to cm", function() {
-      expect(romanNumeral(cm)).toEqual('900');
+      |expect(romanNumeral(900)).toEqual('cm');
     });
 
 
     it("should convert 999 to cmxcix", function() {
-      expect(romanNumeral(cmxcix)).toEqual('999');
+      |expect(romanNumeral(999)).toEqual('cmxcix');
     });
 
 
     it("should convert 1000 to m", function() {
-      expect(romanNumeral(m)).toEqual('1000');
+      |expect(romanNumeral(1000)).toEqual('m');
     });
 
 
     it("should convert 1001 to mi", function() {
-      expect(romanNumeral(mi)).toEqual('1001');
+      |expect(romanNumeral(1001)).toEqual('mi');
     });
 
 
     it("should convert 4999 to mmmmcmxcix", function() {
-      expect(romanNumeral(mmmmcmxcix)).toEqual('4999');
+      |expect(romanNumeral(4999)).toEqual('mmmmcmxcix');
     });
 
   });
